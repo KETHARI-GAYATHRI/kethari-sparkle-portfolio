0a7f2ff076833277ce73dfa19ea30293e597a7a9
Kethari-Gayathri <ketharigayathri123@gmail.com>
2025-08-24 17:48:11 +0530

    ready to build

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:'kethari-sparkle-portfolio',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
